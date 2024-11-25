db07aa38a23b06dd8cd813f5ce0f266ffdeb2e17
DanPaul12 <dan.paul.schechter@gmail.com>
2024-11-25 13:11:34 -0800

    cw2

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class DevelopmentConfig:
-    SQLALCHEMYY_DATABASE_URI = 'mysql+mysqlconnector://root:thegoblet2@localhost/e_commerce_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:thegoblet2@localhost/e_commerce_db'
     DEBUG = True
