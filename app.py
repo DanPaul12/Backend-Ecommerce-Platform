e3b394df3aa86c81dac11f4b4f98bb0b14984cc8
DanPaul12 <dan.paul.schechter@gmail.com>
2024-12-01 20:49:09 -0800

    cw2

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     
 
     with app.app_context():
-        #db.drop_all()
+        db.drop_all()
         db.create_all()
         init_roles_data()
         init_customers_info_data
