9014072a4de779e3619f1fde0362ec9d21e897eb
DanPaul12 <dan.paul.schechter@gmail.com>
2024-12-11 22:14:28 -0800

    setting up postgres

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:thegoblet2@localhost/advanced_e_commerce_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://example_sum_postgres_3qkn_user:nSkwUR0ygS2dEEWA09qgGV9jBApRe8To@dpg-ctd3aijqf0us73bk5080-a.oregon-postgres.render.com/example_sum_postgres_3qkn'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     CACHE_TYPE = 'SimpleCache'
     DEBUG = True
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
