2baf35b08956d9b9f22d08c141ff3121fcb988a3
DanPaul12 <dan.paul.schechter@gmail.com>
2024-12-29 16:57:54 -0800

    merging

--- models/order.py
+++ models/order.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 import datetime
 from typing import List
 from models.orderProduct import order_product
-#from models.product import Product
 
 class Order(Base):
     __tablename__ = "Orders"
